784917bba99a19f37a3ee49a7afff4fe43c381b0
askdfjlas <kaihengd@gmail.com>
2021-01-21 16:51:57 -0500

    Added mt917159878931 to starter.cppp, commented out by default

--- starter.cpp
+++ starter.cpp
@@ -10,6 +10,7 @@ typedef pair<int,int> ii;
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
+  // mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 }
 
 // UPDATE N WHEN UR DONE AAAAAAAAAAAAAAAAAAAAAAAAAAA (if it applies lol)
