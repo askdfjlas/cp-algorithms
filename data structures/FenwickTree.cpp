76ffc8f43fd327ea26b6b7916794a8aa544b7943
askdfjlas <kaihengd@gmail.com>
2022-04-13 20:29:08 -0400

    Make FenwickTree more beautiful

--- data structures/FenwickTree.cpp
+++ data structures/FenwickTree.cpp
@@ -1,71 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <class T>
-struct FenwickTree {
-  T* tree;
-  T mxbit = 1;
-  int size;
-  
-  T prefixSum(int i) {
-    i++;
-    T ret = 0;
-    while(i > 0) {
-      ret += tree[i];
-      i -= (i & -i);
-    }
-    return ret;
+template <typename T> struct FenwickTree {
+  int size = 1, high_bit = 1;
+  vector<T> tree;
+  FenwickTree(int _size) : size(_size) {
+    tree.resize(size + 1);
+    while((high_bit << 1) <= size) high_bit <<= 1;
   }
-  
-  T rangeSum(int l, int r) {
-    return prefixSum(r) - prefixSum(l - 1);
+  FenwickTree(vector<T>& arr) : FenwickTree(arr.size()) {
+    for(int i = 0; i < size; i++) update(i, arr[i]); 
   }
-  
-  int lbound(T x) {
-    int res = 0;
-    T curr = 0;
-    T bit = mxbit;
-    while(bit) {
-      if((res|bit) <= size && curr + tree[res|bit] < x) {
-        res |= bit;
-        curr += tree[res];
+  int lower_bound(T x) {
+    int res = 0; T cur = 0;
+    for(int bit = high_bit; bit > 0; bit >>= 1) {
+      if((res|bit) <= size && cur + tree[res|bit] < x) {
+        res |= bit; cur += tree[res];
       }
-      bit >>= 1;
     }
     return res;
   }
-  
-  void update(int i, T delta) {
-    i++;
-    while(i <= size) {
-      tree[i] += delta;
-      i += (i & -i); 
-    }
+  T prefix_sum(int i) {
+    T ret = 0;
+    for(i++; i > 0; i -= (i & -i)) ret += tree[i];
+    return ret;
   }
-  
-  FenwickTree(int sz) {
-    size = sz;
-    tree = new T[size+1];
-    memset(tree, 0, (size+1)*sizeof(T));
-    while((mxbit << 1) <= size) mxbit <<= 1;
-  }
-  
-  FenwickTree(vector<T>& arr) : FenwickTree(arr.size()) {
-    for(int i = 0; i < size; i++)
-      update(i, arr[i]); 
-  }
+  T range_sum(int l, int r) { return (l > r) ? 0 : prefix_sum(r) - prefix_sum(l - 1); }
+  void update(int i, T delta) { for(i++; i <= size; i += (i & -i)) tree[i] += delta; }
 };
 
 int main() {
   // Testing
-  vector<int> test = {-23, 88, 15, 99, 100, -1000, 2323, -3, 0, 895};
-
+  vector<int> test = {-23, 88, 15, 99, 100, 1000, 2323, 3, 0, 895};
   FenwickTree<int> tree = FenwickTree<int>(test);
   
-  cout << tree.lbound(-23) << ' ' << tree.lbound(-22) << '\n';
-  cout << tree.prefixSum(0) << ' ' << tree.prefixSum(5) << ' ' << tree.prefixSum(8) << '\n';
+  assert(tree.lower_bound(-23) == 0);
+  assert(tree.lower_bound(88) == 3);
+  
+  // Too lazy rn to turn these into asserts :^)
+  cout << tree.prefix_sum(0) << ' ' << tree.prefix_sum(5) << ' ' << tree.prefix_sum(8) << '\n';
   tree.update(0, 100);
-  cout << tree.prefixSum(5) << ' ' << tree.prefixSum(8) << '\n';
+  cout << tree.prefix_sum(5) << ' ' << tree.prefix_sum(8) << '\n';
   tree.update(8, -5235);
-  cout << tree.prefixSum(5) << ' ' << tree.prefixSum(8) << '\n';
+  cout << tree.prefix_sum(5) << ' ' << tree.prefix_sum(8) << '\n';
 }
