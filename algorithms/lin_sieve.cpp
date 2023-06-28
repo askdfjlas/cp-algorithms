f59e37f92f236d1100656eab0248e66253953d19
askdfjlas <kaihengd@gmail.com>
2023-06-28 17:42:16 -0400

    :exploding_head:

---
+++ algorithms/lin_sieve.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(x,n) for(int x=0;x<n;x++)
+#define FORR(x,a,b) for(int x=a;x<b;x++)
+using ll = long long;
+using ii = pair<int,int>;
+
+const int MX = 1e5 + 10;
+int minp[MX];
+vector<int> primes;
+
+void lin_sieve() {
+  FORR(i,2,MX) {
+    if(!minp[i]) {
+      minp[i] = i;
+      primes.push_back(i);
+    }
+    for(int p : primes) {
+      if(p > minp[i] || p * i >= MX) break;
+      minp[p * i] = p; 
+    }
+  }
+}
