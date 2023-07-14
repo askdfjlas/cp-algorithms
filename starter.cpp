54a790067d7dafa471d244d34b6f0c4020be3800
askdfjlas <kaihengd@gmail.com>
2023-07-14 00:11:05 -0400

    =

--- starter.cpp
+++ starter.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 #define FOR(x,n) for(int x=0;x<n;x++)
-#define FORR(x,a,b) for(int x=a;x<b;x++)
+#define FORR(x,a,b) for(int x=a;x<=b;x++)
 using ll = long long;
 using ii = pair<int,int>;
 
